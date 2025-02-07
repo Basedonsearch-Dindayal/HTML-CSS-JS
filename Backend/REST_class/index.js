d8ee83f4a5e46e0cd9b94666a10e9a8d481eb806
Basedonsearch-Dindayal <dindayalsinha301@gmail.com>
2025-02-07 23:14:01 +0530

    add delete post method

--- Backend/REST_class/index.js
+++ Backend/REST_class/index.js
@@ -72,10 +72,10 @@ app.get("/posts/:id/edit",(req,res)=>{
     res.render("edit.ejs", { post });
 });
 
-app.delete("/posts/:id/delete",(req,res)=>{
+app.delete("/posts/:id/",(req,res)=>{
     let{id} = req.params;
-    let post = posts.find((p)=>id == p.id);
-    res.send("Delete successfull");
+    posts = posts.filter((p)=>id !== p.id);
+    res.redirect("/posts");
 });
 
 app.listen(port, () => {
