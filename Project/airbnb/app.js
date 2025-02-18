c0847bf133795e809ac74cc29ab6865623a98a24
Basedonsearch-Dindayal <dindayalsinha301@gmail.com>
2025-02-18 11:19:45 +0530

    Perform all CRUD activity by using restFull

--- Project/airbnb/app.js
+++ Project/airbnb/app.js
@@ -3,10 +3,12 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/Wonderlust");
@@ -28,20 +30,48 @@ app.get("/", (req, res) => {
 // All Listings Route
 app.get("/listings", async (req, res) => {
     const allListings = await Listing.find({});
-    res.render("listings/index", { allListings });
+    res.render("listings/index.ejs", { allListings });
 });
 
-//new create route
-app.get("/listing/new",(res,req)=>{
-    res.render("new");
-})
+//new route
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
+});
 
 // All Show Route
 app.get("/listings/:id", async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
-    res.render("listings/show",{listing});
+    res.render("listings/show.ejs",{listing});
 });
+
+//Create route
+app.post("/listings",async(req,res)=>{
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+})
+
+//edit route
+app.get("/listings/:id/edit",async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing})
+})
+//update route
+app.put("/listings/:id/",async(req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
+
+//Delete route
+app.delete("/listings/:id",async(req,res)=>{
+    let{id} =req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+    console.log("deletedListing")
+})
 
 
 app.listen(8080, () => {
