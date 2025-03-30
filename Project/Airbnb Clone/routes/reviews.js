c61fde1d9de36f024bad6a24ef3ca298470811b5
Basedonsearch-Dindayal <dindayalsinha301@gmail.com>
2025-03-30 11:40:33 +0530

    Implement MVC framework

--- Project/Airbnb Clone/routes/reviews.js
+++ Project/Airbnb Clone/routes/reviews.js
@@ -6,32 +6,12 @@ const wrapAsync = require("../utils/wrapasync");
 const ExpressError = require("../utils/ExpressError");
 const {validateReview,isLoggedIn,isReviewAuthor} = require("../middleware.js")
 
+const reviewController = require("../controllers/reviews.js");
+
 // Create Review Route
-router.post("/", validateReview,isLoggedIn, wrapAsync(async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-    newReview.author = req.user._id;
-    listing.reviews.push(newReview);
-
-
-    await newReview.save();
-    await listing.save();
-    req.flash("success","New Review Created!");
-    res.redirect(`/listings/${listing._id}`);
-}));
+router.post("/", validateReview,isLoggedIn, wrapAsync(reviewController.renderNewReview));
 
 // Delete Review Route
-router.delete("/:reviewId",isLoggedIn,isReviewAuthor, wrapAsync(async (req, res) => { 
-    const { id, reviewId } = req.params;
-
-    let listing = await Listing.findById(id);
-    if (!listing) {
-        throw new ExpressError(404, "Listing not found");
-    }
-    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash("success","Review Deleted!");
-    res.redirect(`/listings/${id}`);
-}));
+router.delete("/:reviewId",isLoggedIn,isReviewAuthor, wrapAsync(reviewController.destroyReview));
 
 module.exports = router;
